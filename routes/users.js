7e9fc1fde24024ef14882db39bc855d7aaec3c33
= <=>
2020-02-09 21:41:49 +0530

    user registeration funactionality updated and store the users in the json file

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-var multer = require('multer')
+const path1 = require('path');
+const fs = require('fs');
+var multer = require('multer');
 
 
 var storage = multer.diskStorage({
@@ -9,7 +10,7 @@ var storage = multer.diskStorage({
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-'+file.originalname)
+    cb(null, Date.now() + '-' + file.originalname)
   }
 })
 
@@ -30,20 +31,30 @@ router.post('/login', function (req, res) {
 
 
 router.post('/register', upload.array('file', 5), function (req, res) {
-let path = {}
-for(const i of req.files)
-{
-	path[i.originalname] = req.rawHeaders[1]+'/uploads/image?image='+i.filename
-}
-console.log(path)
-if(req.files){
-   res.status(200).json({
-url:path
-})
-}
-else{
-res.status(400).send("error")
-}
+  let path = {}
+  for (const i of req.files) {
+    path[i.originalname] = req.rawHeaders[1] + '/uploads/image?image=' + i.filename
+  }
+  fs.readFile(path1.join(__dirname, '../mockfile', 'users.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err.message)
+    } else {
+      obj = JSON.parse(data); //now it an object
+      obj.user.push({
+        username: req.body.username,
+        password: req.body.password,
+        pictures: path
+      });
+      json = JSON.stringify(obj);
+      fs.writeFile(path1.join(__dirname, '../mockfile', 'users.json'), json, 'utf8', (callback) => {
+        console.log("data is written :-", callback)
+      });
+      res.status(200).json({
+        message: "successfully registered"
+      })
+    }
+  });
 });
 
 module.exports = router;
